7b03fbccdc09cb1c3b1ab7faba899b6b5763c789
vishwaCR746 <vishwav671@gmail.com>
2025-03-27 22:16:08 +0530

    updated  fail

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div class="cat" className='text-center'>
+      <p class="loading-text">Loading</p>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+      <div class="cat__segment"></div>
+    </div>
+    
+  )
+};
+export default App
